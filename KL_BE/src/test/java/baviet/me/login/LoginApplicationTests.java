b061cbb10cc91996d46c4d46fb9dd1b00001f904
Van Ba Viet <baviet19@vanbaviet.onmicrosoft.com>
2023-07-03 15:34:58 +0700

    Hello world

---
+++ KL_BE/src/test/java/baviet/me/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package baviet.me.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
